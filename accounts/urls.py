3d21978dacbd758a9d2f98e33be55076cff34d42
Jin Kang <kbjss071@gmail.com>
2023-03-27 23:32:14 -0400

    login redirect and logout redirect

--- accounts/urls.py
+++ accounts/urls.py
@@ -5,7 +5,7 @@ from . import views
 app_name = 'accounts'
 
 urlpatterns = [
-    path('login/', auth_views.LoginView.as_view(),name='login'),
+    path('login/', auth_views.LoginView.as_view(template_name='registration/login.html'),name='login'),
     path('logout/', auth_views.LogoutView.as_view(), name="logout"),
     path('signup/', views.SignUp.as_view(), name="signup"),
     path('profile/<username>/<int:pk>/', views.Profile.as_view(), name="profile")
--- accounts/views.py
+++ accounts/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, redirect
 
 # Create your views here.
-from django.contrib.auth import login, logout
+from django.contrib.auth import login, logout, views
 from django.urls import reverse_lazy
 from django.views.generic import CreateView, DetailView
 from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
