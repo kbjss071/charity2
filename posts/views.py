7f44903fb9625c0081dd2a975a94b778f15b1971
Jin Kang <kbjss071@gmail.com>
2023-04-11 23:42:12 -0400

    continue on post views

--- posts/views.py
+++ posts/views.py
@@ -1,10 +1,18 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 from django.views.decorators.http import require_POST
-from django.http import JsonResponse
-from .models import Post
+from django.http import JsonResponse, Http404
 from comments.forms import CommentForm
-from django.contrib.auth.mixins import LoginRequiredMixin
 from django.views import generic
+from django.contrib.auth import get_user_model
+from django.contrib import messages
+
+User = get_user_model
+
+from django.contrib.auth.decorators import login_required
+from django.views.decorators.http import require_http_methods
+
+from .models import Post
+from .forms import PostForm
 
 from braces.views import SelectRelatedMixin
 
@@ -41,15 +49,60 @@ class PostList(SelectRelatedMixin, generic.ListView):
     select_related=("user")
 
 
-class UserPosts(generic.ListView):
-    model = Post
-    template_name = "posts/user_post_list.html"
+def user_posts(request, username):
+    try:
+        post_user = User.objects.prefetch_related("posts").get(
+            username__iexact=username
+        )
+    except User.DoesNotExist:
+        raise Http404("User does not exist")
 
-    def get_queryset(self):
-        try:
-            self.post_user = User.objects.prefetch_related("posts").get(
-                username__iexact = self.kwargs.get("username")
-            )
-        except User.DoesNotExist:
-            raise
-        return super().get_queryset()
+    posts = post_user.posts.all()
+
+    context = {"post_user": post_user, "posts": posts}
+    return render(request, "posts/user_post_list.html", context)
+    
+
+@login_required
+@require_http_methods(["GET", "POST"])
+def create_post(request):
+    if request.method == 'POST':
+        form = PostForm(request.POST)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.user = request.user
+            post.save()
+            return redirect('post_detail', pk=post.pk)
+    else:
+        form = PostForm()
+
+    return render(request, 'myapp/post_form.html', {'form': form})
+
+
+def create_post(request):
+    if request.method == "POST":
+        form = PostForm(request.POST)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.user = request.user
+            post.save()
+            messages.success(request, "Post created!")
+            return redirect("posts:all")
+    else:
+        form = PostForm()
+
+    return render(request, "posts/post_form.html", {"form": form})
+
+
+def delete_post(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+
+    if request.user != post.user:
+        raise Http404
+
+    if request.method == "POST":
+        post.delete()
+        messages.success(request, "Post deleted!")
+        return redirect("posts:all")
+
+    return render(request, "posts/post_confirm_delete.html", {"object": post})
