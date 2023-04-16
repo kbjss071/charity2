e51226a23d2bdf3cbd6592a6efe84ce35a37ee77
Jin Kang <kbjss071@gmail.com>
2023-04-15 22:09:28 -0400

    start fixing error in user profile page

--- accounts/urls.py
+++ accounts/urls.py
@@ -8,7 +8,7 @@ urlpatterns = [
     path('login/', auth_views.LoginView.as_view(template_name='registration/login.html'),name='login'),
     path('logout/', auth_views.LogoutView.as_view(), name="logout"),
     path('signup/', views.SignUp.as_view(), name="signup"),
-    path('profile/<username>/<int:pk>/', views.Profile.as_view(), name="profile")
+    path('profile/<username>/', views.Profile.as_view(), name="profile")
 
     # path('password_change/', name="password_change"),
     # path('password_change/done', name="password_change_done"),
